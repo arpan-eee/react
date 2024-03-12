c768a70936920caa46cd4b3c38b00e11bb3ac5a0
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-03-12 20:34:31 +0600

    react deploy

--- explore-react-core-concepts-recap/src/Post.jsx
+++ explore-react-core-concepts-recap/src/Post.jsx
@@ -1,13 +1,17 @@
-import './Post.css'
-export default function Post({post}){
-    const {title, body, id, userId} = post;
-    return (
-        <div className='post'>
-            <h5>Title: {title}</h5>
-            <p><small>userId: {userId}</small></p>
-            <p><small>postId: {id}</small></p>
-            <p>{body}</p>
+import "./Post.css";
+export default function Post({ post }) {
+  const { title, body, id, userId } = post;
 
-        </div>
-    )
+  return (
+    <div className="post">
+      <h5>Title: {title}</h5>
+      <p>
+        <small>userId: {userId}</small>
+      </p>
+      <p>
+        <small>postId: {id}</small>
+      </p>
+      <p>{body}</p>
+    </div>
+  );
 }
